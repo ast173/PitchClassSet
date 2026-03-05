df431d6791219cd3fef3c4d9ee0c7c8d4094d596
ast173 <bowen.zhange2@gmail.com>
2026-03-04 23:18:42 -0500

    Refactored code for manual input. Removed time based flat vs b note distinction.

--- modules/keyboard.js
+++ modules/keyboard.js
@@ -1,6 +1,6 @@
 console.log("==================== KEYBOARD ====================");
 
-import { noteToPC, FLAT_TIME_MS } from "./util.js";
+import { noteToPC } from "./util.js";
 import { forteToPrime } from "./maps.js";
 import { undo, redo, remember, rememberAll, getDifference } from "./history.js";
 import { input, calculate, onReset, getPCS } from "./index.js";
@@ -15,8 +15,6 @@ console.log("Imported items from \"./loadSettings.js\"");
 export { parseManualInput }; // to "./index.js"
 
 // ==================== KEYBOARD ====================
-// private variable
-let lastInputTime = 0;
 // private variable
 let lastNote = "";
 document.addEventListener("keydown", e => {
@@ -38,14 +36,13 @@ document.addEventListener("keydown", e => {
     }
 
     // letter inputs
-    if (/^[acdefg]$/i.test(key) || (key === "b" && (!lastNote || Date.now() - lastInputTime >= FLAT_TIME_MS))) {
+    if (/^[a-g]$/i.test(key)) {
         let pc = noteToPC(key);
         toggle(pc);
         remember(pc);
         calculate(false);
         lastNote = key;
-        lastInputTime = Date.now();
-    } else if (/^[#b+\-]$/.test(key) && lastNote) {
+    } else if (/^[#+\-]$/.test(key) && lastNote) {
         let pcs = [noteToPC(lastNote), noteToPC(lastNote, key)];
         toggleAll(pcs);
         rememberAll(pcs);
@@ -94,24 +91,58 @@ document.addEventListener("DOMContentLoaded", () => {
 function parseManualInput() {
     let text = input.value.toLowerCase().trim();
 
-    let forte = (text.toUpperCase().match(/^(?:1[01]|[0-9])-Z?(?:\d\d|\d)$/) || [])[0];
-    if (forte) {
-        let key = forteToPrime.has(forte) ? forte : forte.replace("-", "-Z")
-        if (forteToPrime.has(key)) {
-            return forteToPrime.get(key).split(", ");
-        }
+    let pcs = tryMatchForte(text);
+
+    if (pcs.length === 0) {
+        pcs = tryMatchPCS(text);
     }
 
-    let pcs = (text.match(/1[01]|[0-9]|t|[a-g][#b+\-]?/g) || []).map(pc => {
-        if (pc === "t") return 10;
-        if (/^[a-g]$/.test(pc)) {
-            return letterToPC.get(pc);
-        }
-        if (/^[a-g][#b+\-]$/.test(pc)) {
-            return mod12(letterToPC.get(pc[0]) + semitoneValue.get(pc[1]));
-        }
-        return parseInt(pc);
-    });
-
     return [...new Set(pcs)];
+}
+
+// private function
+// tryMatchForte(String) -> PCS
+function tryMatchForte(text) {
+    let forte = (text.match(/^(1[01]|[0-9])-(z?)(\d{1,2})$/) || [])[0];
+    if (!forte) return [];
+
+    if (forteToPrime.has(forte)) {
+        return forteToPrime.get(forte).split(", ").map(s => parseInt(s));
+    }
+
+    forte = forte.replace(/-(?!z)/, "-z");
+    if (forteToPrime.has(forte)) {
+        return forteToPrime.get(forte).split(", ").map(s => parseInt(s));
+    }
+
+    return []
+}
+
+// private function
+// tryMatchPCS(String) -> PCS
+function tryMatchPCS(text) {
+    let pcs = (text.match(/1[01]|[0-9]|t|[a-g][#b+\-]?/g) || []).map(s => stringToPC(s));
+    return pcs;
+}
+
+// private function
+// stringToPC(String) -> PC
+function stringToPC(string) {
+    if (0 <= string && string <= 11) {
+        return string;
+    }
+
+    if (/^[a-g]$/.test(string)) {
+        return noteToPC(string);
+    }
+    if (/^[a-g][#b+\-]$/.test(string)) {
+        return noteToPC(string[0], string[1]);
+    }
+
+    if (string === "t") return 10;
+    if (/^1[01]|[0-9]$/.test(string)) {
+        return parseInt(string);
+    }
+
+    throw console.error(`The string \"${string}\" cannot be converted to a PC`);
 }
--- modules/util.js
+++ modules/util.js
@@ -4,7 +4,7 @@ console.log("\"./util.js\" has no imports");
 export { UNORDERED, NORMAL_ORDER, PRIME_FORM, IC_VECTOR }; // to "./index.js"
 export { FORTE, RAHN, mod12 }; // to "./deep.js"
 export { MAX_SEMITONES }; // to "./index.js"
-export { noteToPC, FLAT_TIME_MS }; // to "./keyboard.js"
+export { noteToPC }; // to "./keyboard.js"
 
 // ==================== UTIL ====================
 const MAX_SEMITONES = 12;
@@ -19,10 +19,12 @@ const NORMAL_ORDER = Symbol("normal_order");
 const PRIME_FORM = Symbol("prime_form");
 const IC_VECTOR = Symbol("ic_vector");
 
+// private map
 let letterToPC = new Map([
     ["a", 9], ["b", 11], ["c", 0], ["d", 2], ["e", 4], ["f", 5], ["g", 7],
 ]);
 
+// private map
 let accedentalToValue = new Map([
     ["#", 1], ["b", -1],
     ["+", 1], ["-", -1],
@@ -34,8 +36,6 @@ let accedentalToValue = new Map([
 function noteToPC(key, accedental = "=") {
     return mod12(letterToPC.get(key) + accedentalToValue.get(accedental));
 }
-
-const FLAT_TIME_MS = 500;
 
 // public function
 // mod12(Integer) -> PC
